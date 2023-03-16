d9a5998dab6dbce83cf3b31edcae15c07187fae9
Mathtuzo <106055417+Mathtuzo@users.noreply.github.com>
2023-03-16 10:14:40 +0100

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,27 @@
+function openGaudi() {
+    document.getElementById('demo-Gaodi').style.display = 'block';
+    document.getElementById('demo-leFoll').style.display = 'none';
+    document.getElementById('JP-Demo').style.display = 'none';
+    document.getElementById('demo-siteDynamique').style.display = 'none';
+}
+
+function openFoll() {
+    document.getElementById('demo-Gaodi').style.display = 'none';
+    document.getElementById('demo-leFoll').style.display = 'block';
+    document.getElementById('JP-Demo').style.display = 'none';
+    document.getElementById('demo-siteDynamique').style.display = 'none';
+}
+
+function openJP() {
+    document.getElementById('demo-Gaodi').style.display = 'none';
+    document.getElementById('demo-leFoll').style.display = 'none';
+    document.getElementById('JP-Demo').style.display = 'block';
+    document.getElementById('demo-siteDynamique').style.display = 'none';
+}
+
+function openSiteDyn() {
+    document.getElementById('demo-Gaodi').style.display = 'none';
+    document.getElementById('demo-leFoll').style.display = 'none';
+    document.getElementById('JP-Demo').style.display = 'none';
+    document.getElementById('demo-siteDynamique').style.display = 'block';
+}
